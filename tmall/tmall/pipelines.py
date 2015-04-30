907cc1b88e9a16205eae9e7847c6ec707be30c23
root <root@localhost.(none)>
2015-04-30 18:23:21 +0800

    add

--- tmall/tmall/pipelines.py
+++ tmall/tmall/pipelines.py
@@ -12,7 +12,7 @@ class TmallPipeline(object):
         conn = MySQLdb.connect(host='192.168.1.123',user='root',passwd='111111',db='photo',charset='utf8')
         cu = conn.cursor()
         if isinstance(item,ProductItem):
-            cu.execute("insert into tmall(productId,name,promote_price,origin_price,send_address,standard,url)values(%s,%s,%s,%s,%s,%s,%s)",(long(item['productId']),item['name'],float(item['promote_price']),item['origin_price'],item['send_address'],item['standard'],item['url']))
+            cu.execute("insert into tmall_tmall(productId,name,promote_price,origin_price,send_address,standard,url)values(%s,%s,%s,%s,%s,%s,%s)",(long(item['productId']),item['name'],float(item['promote_price']),item['origin_price'],item['send_address'],item['standard'],item['url']))
         cu.execute("commit")
         return item
 
